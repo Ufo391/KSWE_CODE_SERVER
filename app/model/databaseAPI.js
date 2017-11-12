ff0ebe8f9792a69caaecd732d7028082cacc757d
Ufo391 <nk-west@hotmail.de>
2017-11-12 11:41:14 +0100

    Dummy DatabaseAPI.js implementiert

---
+++ app/model/databaseAPI.js
@@ -0,0 +1,12 @@
+var fs = require("fs");
+
+function read(req, res){
+
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+        console.log( data );
+        res.end( data );
+    });
+
+}
+
+module.exports.read = read;
--- server.js
+++ server.js
@@ -1,21 +1,29 @@
+// dependencies
 var express = require('express');
-var app = express();
-var fs = require("fs");
 
+var db = require('./app/model/databaseAPI')
+
+// instances
+var app = express();
+
+
+// attributes
 var server_port = 3000;
 
-app.get('/listUsers', function (req, res) {
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       console.log( data );
-       res.end( data );
-   });
-})
+
+// functions
 
 var server = app.listen(server_port, function () {
 
   var host = server.address().address
   var port = server.address().port
 
-  console.log("Example app listening at http://%s:%s", host, port)
+  console.log("Server listening at http://%s:%s", host, port)
 
+})
+
+// routes
+
+app.get('/login', function (req, res) {
+    db.read(req,res);
 })
